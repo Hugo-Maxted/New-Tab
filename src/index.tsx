a4e40c30042f229fa2460a63a3b5c9d1d7d48af9
HuggyDaBuggy <hugo@maxtedfamily.net>
2021-08-17 13:25:42 +0800

    stuff

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,30 @@
 import React from "react";
-import {render} from "react-dom";
+import { useEffect } from "react";
+import { useState } from "react";
+import { render } from "react-dom";
 import "./main.css";
+
+const UI: () => JSX.Element = () => {
+  const [time, setTime] = useState<Date>(new Date());
+
+  useEffect((): void => {
+    setInterval((): void => {
+      setTime(new Date());
+    }, 1000);
+  }, []);
+
+  return (
+    <div className="ui">
+      <div className="time">
+        {time.getHours()}:{time.getMinutes()}:{time.getSeconds()}
+      </div>
+    </div>
+  );
+};
 
 render(
   <React.StrictMode>
-    <div className="content center">content moment</div>
-    <div className="settings">
-      <i className="fas fa-cog"></i>
-    </div>
+    <UI />
   </React.StrictMode>,
   document.getElementById("root")
 );

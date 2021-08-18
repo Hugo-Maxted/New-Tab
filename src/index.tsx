08c4455a42a920466c296a8aa04153f772645892
HuggyDaBuggy <hugo@maxtedfamily.net>
2021-08-18 09:31:14 +0800

    weatherr

--- src/index.tsx
+++ src/index.tsx
@@ -13,28 +13,52 @@ interface linkType {
 
 const UI: () => JSX.Element = () => {
   const [time, setTime] = useState<Date>(new Date());
+  const [weather, setWeather] = useState<any>(null);
 
   let links: linkType = {
-    MEDIA: [{name: "Youtube", link: "https://www.youtube.com"}],
-    SCHOOL: [
+    Media: [{name: "Youtube", link: "https://www.youtube.com"}],
+    School: [
       {name: "Connect", link: "https://connect.det.wa.edu.au/group/students/ui/overview"},
       {name: "Mathspace", link: "https://mathspace.co/student/"},
       {name: "Stile App", link: "https://stileapp.com/au/PMS_WA-988"},
     ],
-    CODING: [{name: "Github", link: "https://github.com/Hugo-Maxted"}],
+    Coding: [{name: "Github", link: "https://github.com/Hugo-Maxted"}],
   };
 
+  let periods: number[][] = [];
+
   useEffect((): void => {
+    async function getWeather(): Promise<any> {
+      let header: Headers = new Headers();
+      header.append("pragma", "no-cache");
+      header.append("cache-control", "no-cache");
+
+      await fetch(new Request("https://api.weatherapi.com/v1/current.json?key=e1830767ff0446e7a47132846211907&q=Perth"), {
+        method: "GET",
+        headers: header,
+      })
+        .then((response: Response): Promise<any> => {
+          return response.json();
+        })
+        .then((data: any): void => {
+          setWeather(data);
+        });
+    }
+
+    getWeather();
+
     setInterval((): void => {
       setTime(new Date());
     }, 1000);
+
+    setInterval(getWeather, 60000);
   }, []);
 
   return (
     <div className="ui">
       <div className="weather">
-        <div>69°C</div>
-        <div>Flood</div>
+        <div>{weather === null ? "Loading..." : weather.current.temp_c + "°C"}</div>
+        <div>{weather === null ? "" : weather.current.condition.text}</div>
       </div>
       <div className="timetable">
         <div>Hass</div>
@@ -45,25 +69,25 @@ const UI: () => JSX.Element = () => {
       </div>
       <div className="time">
         <div className="time">
-        {time.getHours()}:{time.getMinutes() < 10 ? "0" : ""}
-        {time.getMinutes()}:{time.getSeconds() < 10 ? "0" : ""}{time.getSeconds()}</div>
+          {time.getHours()}:{time.getMinutes() < 10 ? "0" : ""}
+          {time.getMinutes()}:{time.getSeconds() < 10 ? "0" : ""}
+          {time.getSeconds()}
+        </div>
         <div className="date">
-          {["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "Friday", "Saturday"][time.getDay()]}, {" "}
-          {time.getDate()}
+          {["Sunday", "Monday", "Teusday", "Wednesday", "Thursday", "Friday", "Saturday"][time.getDay()]}, {time.getDate()}
           {time.getDate() === 1 ? "st" : time.getDate() === 2 ? "nd" : time.getDate() === 3 ? "rd" : "th"} of{" "}
           {["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][time.getMonth()]} {time.getFullYear()}
         </div>
       </div>
-      <div className="links" style={{gridTemplateColumns: "1fr " + "auto 50px ".repeat(Object.keys(links).length - 1) + "auto 1fr"}}>
-        <div></div>
+      <div className="links" style={{gridTemplateColumns: "auto 50px ".repeat(Object.keys(links).length - 1) + "auto"}}>
         {Object.keys(links).map(
           (catagory: string): JSX.Element => (
             <>
               <div className="catagory">
-                <div className="title">{catagory}</div>
+                <div className="title">{catagory.toUpperCase()}</div>
                 {links[catagory].map(
                   (link: {name: string; link: string}): JSX.Element => (
-                      <a href={link.link}>{link.name}</a>
+                    <a href={link.link}>{link.name}</a>
                   ),
                 )}
               </div>
